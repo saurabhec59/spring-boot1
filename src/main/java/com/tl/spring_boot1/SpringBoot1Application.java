d666e199570b271ae9852523101d6c86fba55bbc
saurabhec59 <saurabhyadav2690@gmail.com>
2026-07-30 22:12:52 +0530

    initial commit

---
+++ src/main/java/com/tl/spring_boot1/SpringBoot1Application.java
@@ -0,0 +1,14 @@
+package com.tl.spring_boot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+    // starting
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+
+}
